4d36799a989100d81f2de3f04a949f84c6c33360
shivampandey9802 <shivampandey9802@gmail.com>
2025-07-16 21:03:16 +0530

    gemini clone

---
+++ src/config/gemini.js
@@ -0,0 +1,56 @@
+// const apikey = "AIzaSyBO9yrD0IC-h2hnO_CbOtiRwj8x9VxdaFY"
+
+
+// To run this code you need to install the following dependencies:
+// npm install @google/genai mime
+// npm install -D @types/node
+
+import {
+  GoogleGenAI,
+} from '@google/genai';
+
+async function main(prompt) {
+  const ai = new GoogleGenAI({
+    apiKey: "AIzaSyBO9yrD0IC-h2hnO_CbOtiRwj8x9VxdaFY",
+  });
+  const tools = [
+    {
+      googleSearch: {
+      }
+    },
+  ];
+  const config = {
+    thinkingConfig: {
+      thinkingBudget: -1,
+    },
+    tools,
+    responseMimeType: 'text/plain',
+  };
+  const model = 'gemini-2.5-pro';
+  const contents = [
+    {
+      role: 'user',
+      parts: [
+        {
+          text: prompt,
+        },
+      ],
+    },
+  ];
+
+  const response = await ai.models.generateContentStream({
+    model,
+    config,
+    contents,
+  });
+  let fileIndex = 0;
+  let fullResponse = '';
+  for await (const chunk of response) {
+    // console.log(chunk.text);
+    fullResponse += chunk.text;
+  }
+  
+  return fullResponse.slice(2,fullResponse.length);
+}
+
+export default main;
